d3105d44df6fbe4a200ab4d7777e44b8aba50916
Andeboizin5 <andeboizin5@gmail.com>
2023-12-12 08:52:04 -0800

    _proud.c

---
+++ _proud.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * pass_betty - beautiful code that passes the Betty checks
+ */
+void pass_betty(void)
+{
+	printf("Check, Betty!\n");
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	pass_betty();
+	return (0);
+}
