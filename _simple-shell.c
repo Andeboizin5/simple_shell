3d9c45f48c19faf50af975d5cc074220db47846a
Andeboizin5 <andeboizin5@gmail.com>
2023-12-13 11:58:33 -0800

    _simple-shell.c

--- _simple-shell.c
+++ _simple-shell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
@@ -13,40 +14,47 @@
 int main(void)
 {
 	char buffer[BUFFER_SIZE];
+	char *args[2];
+	ssize_t read_chars;
 	pid_t child_pid;
 	int status;
 
 	while (1)
 	{
 		printf("#cisfun$ ");
-		if (!fgets(buffer, BUFFER_SIZE, stdin))
+
+		if ((read_chars = read(STDIN_FILENO, buffer, BUFFER_SIZE)) == -1)
+		{
+			perror("read");
+			return (EXIT_FAILURE);
+		}
+
+		if (read_chars == 0)
 		{
 			printf("\n");
 			break;
 		}
 
-		buffer[strcspn(buffer, "\n")] = 0;
+		buffer[read_chars - 1] = '\0';
+		args[0] = buffer;
+		args[1] = NULL;
 
-		child_pid = fork();
-
-		if (child_pid == -1)
+		if ((child_pid = fork()) == -1)
 		{
-			perror("Error forking");
-			exit(EXIT_FAILURE);
+			perror("fork");
+			return EXIT_FAILURE;
 		}
 
 		if (child_pid == 0)
 		{
-			if (execve(buffer, NULL, NULL) == -1)
+			if (execve(args[0], args, NULL) == -1)
 			{
-				perror("Execution error");
-				exit(EXIT_FAILURE);
+				perror("execve");
+				return EXIT_FAILURE;
 			}
-		} else
-		{
+		} else {
 			waitpid(child_pid, &status, 0);
 		}
 	}
-	return (EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
-
